1165f284eb0af75fd91cd63a6aa41433054d40b3
Divya <divya.raphy@dci-student.org>
2024-11-05 09:00:46 +0100

    msg

--- task-11.js
+++ task-11.js
@@ -1,1 +1,23 @@
 
+// Create a function that concatenates n input arrays, where n is variable.
+
+// Examples
+// concat([1, 2, 3], [4, 5], [6, 7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+
+// concat([1], [2], [3], [4], [5], [6], [7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+
+// concat([1, 2], [3, 4]) ➞ [1, 2, 3, 4]
+
+// concat([4, 4, 4, 4, 4]) ➞ [4, 4, 4, 4, 4]
+
+function concat(...args) {
+    let result = [];
+    for (let i = 0; i < args.length; i++) {
+        result = result.concat(args[i]);
+    }
+    return result;
+}
+
+console.log(concat([1, 2, 3], [4, 5], [6, 7])); // [1, 2, 3, 4, 5, 6, 7]
+
+console.log(concat([1], [2], [3], [4], [5], [6], [7])); // [1, 2, 3, 4, 5, 6, 7]
---
+++ task-13.js
@@ -0,0 +1,23 @@
+// Create a function that counts the integer's number of digits.
+
+// Examples
+// count(318) ➞ 3
+ 
+// count(-92563) ➞ 5
+ 
+// count(4666) ➞ 4
+
+// count(-314890) ➞ 6
+
+// count(654321) ➞ 6
+
+// count(638476) ➞ 6
+
+function count(n) {
+
+    return Math.abs(n).toString().length;
+}
+
+console.log(count(318)); // 3
+
+console.log(count(-92563)); // 5
